0bcd1b684e998e21ecaf2967bed6a14c2f876f29
Srinivasaimandi <srinivasaimandi@gmail.com>
2017-08-08 12:22:11 +0530

    new format
    New format implemented in writing page objects and writing test scripts

--- pages/multiform.js
+++ pages/multiform.js
@@ -1,7 +1,21 @@
-module.exports = function(){
+// module.exports = function(){
+//
+// this.iptName = element(by.model('formData.name'));
+// this.iptEmail = element(by.model('formData.email'));
+// this.btnNextSection = element(by.className('btn btn-block btn-info'));
+// this.getElement = function (str) {
+//   var xp = ".//label[text()[contains(.,'" + str + "')]]/input";
+//   return element(by.xpath(xp));
+// };
+// }
 
-this.iptName = element(by.model('formData.name'));
-this.iptEmail = element(by.model('formData.email'));
-this.btnNextSection = element(by.className('btn btn-block btn-info'));
 
+module.exports = {
+  iptName: element(by.model('formData.name')),
+  iptEmail : element(by.model('formData.email')),
+  btnNextSection : element(by.className('btn btn-block btn-info')),
+  getElement : function (str) {
+    var xp = ".//label[text()[contains(.,'" + str + "')]]/input";
+    return element(by.xpath(xp));
+  }
 }
--- specs/form_spec.js
+++ specs/form_spec.js
@@ -3,13 +3,13 @@ var multi_form = require('../pages/multiform.js');
 describe('form', function() {
 
   var EC;
-  var page;
+  var page = multi_form;
   var time_out = 10000;
 
   beforeEach(function() {
     browser.get('http://www.way2automation.com/angularjs-protractor/multiform/#/form/profile')
     EC = protractor.ExpectedConditions;
-    page = new multi_form();
+    //page = new multi_form();
   });
 
   it('test1', function() {
@@ -18,5 +18,6 @@ describe('form', function() {
       page.iptEmail.sendKeys('Sandy@gmail.com');
       page.btnNextSection.click();
       browser.wait(EC.visibilityOf(page.btnNextSection), time_out);
+      page.getElement('I like PS4').click();
   });
 });
