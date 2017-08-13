3676fff359237919bd65aed00c347012658baca4
Srinivasaimandi <srinivasaimandi@gmail.com>
2017-08-13 20:40:37 +0530

    ATOM is defaulted as Editor to be used

--- pages/phptravels/phplogin.js
+++ pages/phptravels/phplogin.js
@@ -1,4 +1,3 @@
 module.exports = {
-    btnMyAccount : element(by.xpath('html/body/div[2]/div/div/div[2]/ul/li[2]/a')),
-    btnLogin : element(by.xpath('html/body/div[2]/div/div/div[2]/ul/li[2]/ul/li[1]/a')),
+
 };
--- specs/phptravels/test_spec.js
+++ specs/phptravels/test_spec.js
@@ -1,21 +1,5 @@
-//var sample = require(protractor.basePath + '/TestBed/phplogin.js');
-var sample = require(protractor.basePath + '/Pages/phptravels/phplogin.js');
-
-describe('main', function() {
-
-var EC;
-var page = sample;
-var time_out = 1000;
-
-beforeEach(function() {
-    page = sample;
-    EC = protractor.ExpectedConditions;
-});
-
-it('login1', function() {
-    browser.get(browser.params.phptravels.baseurl);
-    page.btnMyAccount.click();
-    page.btnLogin.click();
-});
-
+describe('blah', function() {
+  it('should blah', function() {
+    browser.get('https://www.lego.com/en-us/');
+  });
 });
